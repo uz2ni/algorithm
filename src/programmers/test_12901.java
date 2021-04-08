961062616ae0ac41bab4d0f72c59e5ee94311170
uz2ni <uzini_@likelion.org>
2021-04-08 12:10:26 +0900

    programmers LV1 - 2016년 (solution2 추가)

--- src/programmers/test_12901.java
+++ src/programmers/test_12901.java
@@ -7,9 +7,11 @@ import java.util.Calendar;
  */
 public class test_12901 {
 	public static void main(String[] args) {
-		System.out.println(solution(5, 24)); // TUE
+	//	System.out.println(solution(5, 24)); // TUE
+		System.out.println(solution2(5, 24));
 	}
 
+	// 1. Calendar API 이용
 	public static String solution(int a, int b) { // 2016년 a월, b일 -> 요일 리턴
 		String answer = "";
 		String[] DAY = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
@@ -24,6 +26,24 @@ public class test_12901 {
 		int idx = cal.get(Calendar.DAY_OF_WEEK);
 
 		answer = DAY[idx-1];
+		return answer;
+	}
+
+	// 1. 계산법 이용
+	public static String solution2(int a, int b) {
+		// 2016. 1. 1 -> FRI
+		String answer = "";
+		int[] MONTH_PLUS = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; // 2016년 2월은 윤달이라 29일
+		String[] DAY = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+		int sum = 0;
+
+		for(int i = 0; i<a-1; i++) {
+			sum += MONTH_PLUS[i];
+		}
+		sum += (b-1);
+
+		answer = DAY[sum%7];
+
 		return answer;
 	}
 }
