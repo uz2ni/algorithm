4b91b80fac359f214c625d9102aafeb9e3bee87d
uz2ni <uzini_@likelion.org>
2021-04-09 17:42:59 +0900

    programmers LV1 - 모의고사 (작성중)

---
+++ src/programmers/test_42748.java
@@ -0,0 +1,25 @@
+package programmers;
+/*
+	모의고사
+ */
+public class test_42748 {
+	public static void main(String[] args) {
+		int[] array = {1, 5, 2, 6, 3, 7, 4};
+		int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+	}
+
+	public static int[] solution(int[] array, int[][] commands) {
+		int[] answer = {};
+
+		// array 갯수만큼 돌리기
+
+		// i~j 번째 자르기
+
+		// 정렬하기
+
+		// k번째 수 answer에 담기
+
+
+		return answer;
+	}
+}
