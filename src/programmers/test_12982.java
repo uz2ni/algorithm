8c8245e0e11cc2d6b4539925a9583c14a85e17ca
uz2ni <uzini_@likelion.org>
2021-04-24 00:58:01 +0900

    programmers LV1 - 예산

---
+++ src/programmers/test_12982.java
@@ -0,0 +1,29 @@
+package programmers;
+
+import java.util.Arrays;
+
+/*
+    예산
+ */
+public class test_12982 {
+    public static void main(String[] args) {
+        int[] d = {2,2,3,3};
+        int budget = 10;
+        System.out.println(solution(d, budget));
+    }
+    static public int solution(int[] d, int budget) {
+        int answer = 0;
+        int sum = 0;
+
+        Arrays.sort(d);
+        for(int i=0; i<d.length; i++) {
+            sum+=d[i];
+            if(sum > budget) {
+                break;
+            }
+            answer++;
+        }
+
+        return answer;
+    }
+}
