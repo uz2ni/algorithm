196dbf926a80c85558407e4590a54080a6c9b181
uz2ni <uzini_@likelion.org>
2021-04-26 11:32:32 +0900

    programmers LV1 - 모의 고사 (작성중)

---
+++ src/programmers/test_42840.java
@@ -0,0 +1,29 @@
+package programmers;
+
+/*
+    모의 고사
+ */
+public class test_42840 {
+    public static void main(String[] args) {
+
+    }
+
+    static public int[] solution(int[] answers) {
+        int[] answer = {};
+
+
+
+        return answer;
+    }
+
+    // 1번 수포자가 찍는 방식 계산하여 맞은 갯수 리턴
+    static public int check1(int[] answers, int[] pattern) { // pattern:12345
+        int cnt = 0;
+
+        for(int i=1; i<=10000; i++) {
+
+        }
+
+        return cnt;
+    }
+}
