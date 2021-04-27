e30de3d218de910ef5f723789e4ffb8aaca54e3c
uz2ni <uzini_@likelion.org>
2021-04-27 10:42:08 +0900

    programmers LV1 - 모의 고사 (완료)

--- src/programmers/test_42840.java
+++ src/programmers/test_42840.java
@@ -8,40 +8,57 @@ import java.util.List;
  */
 public class test_42840 {
     public static void main(String[] args) {
-        int[] answers = {1,2,3,4,5};
-        solution(answers);
+        int[] answers = {1,3,2,4,2};
+        int[] result = solution(answers);
+        for(int i=0; i<result.length; i++) {
+            System.out.print(result[i] + ", ");
+
+        }
 
     }
 
     static public int[] solution(int[] answers) {
         int[] pattern1 = {1,2,3,4,5};
-        int[] pattern2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] pattern3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int[] cnt = {0,0,0};
+        int[] pattern2 = {2,1,2,3,2,4,2,5};
+        int[] pattern3 = {3,3,1,1,2,2,4,4,5,5};
+        int answer1=0, answer2 =0, answer3 =0;
         List<Integer> result = new ArrayList<>();
 
         for(int i=0; i<answers.length; i++) {
-            if(answers[i % answers.length] == pattern1[i % pattern1.length]) cnt[0]++;
-            if(answers[i % answers.length] == pattern2[i % pattern1.length]) cnt[1]++;
-            if(answers[i % answers.length] == pattern3[i % pattern1.length]) cnt[2]++;
+            if(pattern1[i % pattern1.length] == answers[i]) answer1++;
+            if(pattern2[i % pattern2.length] == answers[i]) answer2++;
+            if(pattern3[i % pattern3.length] == answers[i]) answer3++;
         }
 
-        // max 구하기
-        int max = cnt[0];
-        for(int i=1; i<=2; i++) {
-            if(cnt[i] > max) max = cnt[i];
-        }
-        // 오름차순
-        for(int i=0; i<3; i++) {
-            if(cnt[i] == max) result.add(i+1);
-        }
+        int max = Math.max(Math.max(answer1, answer2), answer3);
+
+        if(answer1 == max) result.add(1);
+        if(answer2 == max) result.add(2);
+        if(answer3 == max) result.add(3);
 
         int[] resultArr = new int[result.size()];
-        for(int i=0; i<result.size(); i++) {
+        for(int i=0; i<resultArr.length; i++) {
             resultArr[i] = result.get(i);
         }
 
-
         return resultArr;
+    }
+
+    static public int[] solution2(int[] answer) {
+        int[] a = {1, 2, 3, 4, 5};
+        int[] b = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] score = new int[3];
+        for(int i=0; i<answer.length; i++) {
+            if(answer[i] == a[i%a.length]) {score[0]++;}
+            if(answer[i] == b[i%b.length]) {score[1]++;}
+            if(answer[i] == c[i%c.length]) {score[2]++;}
+        }
+        int maxScore = Math.max(score[0], Math.max(score[1], score[2]));
+        ArrayList<Integer> list = new ArrayList<>();
+        if(maxScore == score[0]) {list.add(1);}
+        if(maxScore == score[1]) {list.add(2);}
+        if(maxScore == score[2]) {list.add(3);}
+        return list.stream().mapToInt(i->i.intValue()).toArray();
     }
 }
