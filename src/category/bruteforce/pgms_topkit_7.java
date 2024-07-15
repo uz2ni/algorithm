fc60fca9856a718cfda913db79aa3a27d6b41c6b
uz2ni <uzini_@naver.com>
2024-07-16 07:23:33 +0900

    완전탐색_프로그래머스_모음사전 (LV2)

---
+++ src/category/bruteforce/pgms_topkit_7.java
@@ -0,0 +1,40 @@
+package category.bruteforce;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/** 84512
+ * 코딩테스트 연습 > 완전탐색 > 모음사전
+ */
+public class pgms_topkit_7 {
+
+    static List<String> list;
+    static String[] words = {"A", "E", "I", "O", "U"};
+
+    public static void main(String[] args) {
+        list = new ArrayList<>();
+        System.out.println(solution("AAAAE"));
+    }
+
+    public static int solution(String word) {
+        int answer = 0;
+        dfs("", 0);
+        // word == list.get(i)
+        for(int i=0; i<list.size(); i++) {
+            if(word.equals(list.get(i))) {
+                answer = i;
+                break;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void dfs(String str, int len) {
+        list.add(str);
+        if(len == words.length) return;
+        for(int i=0; i<words.length; i++) {
+            dfs(str + words[i], len+1);
+        }
+    }
+}
