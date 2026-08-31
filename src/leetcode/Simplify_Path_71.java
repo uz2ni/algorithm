be4662484203cf31da0ade7a519aabd4df84a399
uz2ni <uzini_@naver.com>
2026-08-31 16:56:26 +0900

    [leetcode] 71. Simplify Path

---
+++ src/leetcode/Simplify_Path_71.java
@@ -0,0 +1,66 @@
+package leetcode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/** 71. Simplify Path **/
+public class Simplify_Path_71 {
+
+    public static void main(String[] args) {
+
+        System.out.println(simplifyPath2("/home/user/Documents/../Pictures/././..")); // 답: /home/user
+    }
+
+    // 문자열 활용 방식
+    public static String simplifyPath(String path) {
+
+        String[] splitStr = path.split("/");
+        StringBuilder sb = new StringBuilder();
+
+        for(String s : splitStr) {
+            if(s.equals("..")) {
+                // sb에서 뒤에서 첫번째 슬래시까지 삭제
+                int idx = sb.lastIndexOf("/");
+                if(idx != -1) {
+                    sb.setLength(idx); // index 부터 잘라냄
+                }
+            }else if(s.equals(".") || s.equals("")) continue; // ".", "" 이면 생략
+            else {
+                sb.append("/").append(s);
+            }
+        }
+
+        return sb.toString().length() == 0 ? "/" : sb.toString();
+    }
+
+    // Stack 사용 방식
+    public static String simplifyPath2(String path) {
+        String[] splitStr = path.split("/");
+        Deque<String> stack = new ArrayDeque<>();
+
+        for(String s : splitStr) {
+            if(s.equals("..")) {
+                if(!stack.isEmpty()) {
+                    stack.pop();
+                }
+            }else if(s.equals(".") || s.equals("")) continue;
+            else {
+                stack.push(s);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()) {
+            // O(N^2) : 앞에 끼워넣으려면 n번 밀고, n개 반복
+            // sb.insert(0, "/"+stack.pop());
+
+            // O(N) : dir 문자열도 뒤집어서 이어붙임, 슬래시도 우측에 붙임
+            String dir = stack.pop();
+            sb.append(new StringBuilder(dir).reverse()).append("/");
+        }
+        // 마지막에 정방향으로 전체 뒤집기
+        sb.reverse();
+
+        return sb.toString().isEmpty() ? "/" : sb.toString();
+    }
+}
