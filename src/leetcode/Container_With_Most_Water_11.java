2c4738b50114bff657d4a59dab2e0aa088c94de7
uz2ni <uzini_@naver.com>
2026-09-22 15:08:43 +0900

    [leetcode] 11. Container With Most Water (3회차)

--- src/leetcode/Container_With_Most_Water_11.java
+++ src/leetcode/Container_With_Most_Water_11.java
@@ -4,7 +4,7 @@ package leetcode;
 public class Container_With_Most_Water_11 {
 
     public static void main(String[] args) {
-        System.out.println(maxArea(new int[]{1,8,6,2,5,4,8,3,7}));
+        System.out.println(maxArea2(new int[]{1,8,6,2,5,4,8,3,7}));
     }
 
     public static int maxArea(int[] height) {
@@ -27,5 +27,22 @@ public class Container_With_Most_Water_11 {
         }
 
         return max;
+    }
+
+    public static int maxArea2(int[] height) {
+        int answer = 0;
+        int left = 0;
+        int right = height.length-1;
+
+        while(left < right) {
+            if(height[left] < height[right]) {
+                answer = Math.max(answer, (right-left) * height[left]);
+                left++;
+            }else {
+                answer = Math.max(answer, (right-left) * height[right]);
+                right--;
+            }
+        }
+        return answer;
     }
 }
